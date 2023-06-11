19d49088714d867a636718e250ce1c40be2af51e
Alamin <alamin017514@gmail.com>
2023-06-11 12:15:10 +0600

    install jwt axios

--- index.js
+++ index.js
@@ -2,11 +2,32 @@ const express = require('express');
 const app = express();
 require('dotenv').config()
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const stripe = require("stripe")(process.env.PAYMENT_SECRET_KEY)
 const port = process.env.PORT || 5000;
 
 // middleware 
 app.use(cors());
 app.use(express.json());
+
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: 'unauthorized access' });
+    }
+    // bearer token
+    const token = authorization.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ error: true, message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
+
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -29,6 +50,15 @@ async function run() {
         const classesCollection = client.db('learninDB').collection('classes');
         const studentsCollection = client.db('learninDB').collection('students');
 
+
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+
+            res.send({ token })
+        })
+
+
         app.get('/classes', async (req, res) => {
             const result = await classesCollection.find().toArray();
             res.send(result)
@@ -46,6 +76,7 @@ async function run() {
             const result = await classesCollection.insertOne(addClass);
             res.send(result)
         })
+
 
         app.put('/students/:id', async (req, res) => {
             const student = req.body
@@ -66,6 +97,18 @@ async function run() {
             const result = await studentsCollection.deleteOne(query)
             res.send(result)
         })
+
+        // payment related api
+        app.post('/payments', verifyJWT, async (req, res) => {
+            const payment = req.body;
+            const insertResult = await paymentCollection.insertOne(payment);
+
+            const query = { _id: { $in: payment.cartItems.map(id => new ObjectId(id)) } }
+            const deleteResult = await cartCollection.deleteMany(query)
+
+            res.send({ insertResult, deleteResult });
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
